a8644fb20091a55361709c36528ce38e5c18c317
woong <lhw2106@naver.com>
2021-07-23 13:00:12 +0900

    add 07/23

--- pybo/views.py
+++ pybo/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def index(request):
@@ -20,15 +20,24 @@ def board(request):
     # 질문 목록
     # 127.0.0.1:8000/pybo/board/?page=1
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
 
     # 조회
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |                      # 제목 검색
+            Q(content__icontains=kw) |                      # 내용 검색
+            Q(author__username__icontains=kw) |             # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw) |     # 답변 글쓴이 검색
+            Q(answer__content__icontains=kw)
+        ).distinct()  # 중복 제거
 
     # 페이지 처리 - 페이지당 10개씩 보여줌
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 
